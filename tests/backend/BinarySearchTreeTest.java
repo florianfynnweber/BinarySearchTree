04e0b73da931bb0a0863fd87176d69b246c0a31d
Florian Weber <fwe@plutex.de>
2019-09-19 11:12:46 +0200

    add tests

---
+++ tests/backend/BinarySearchTreeTest.java
@@ -0,0 +1,36 @@
+package backend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchTreeTest {
+
+    @Test
+    void addValue() {
+    }
+
+    @Test
+    void testAddValue() {
+    }
+
+    @Test
+    void delValue() {
+    }
+
+    @Test
+    void hasValue() {
+    }
+
+    @Test
+    void search() {
+    }
+
+    @Test
+    void getDepth() {
+    }
+
+    @Test
+    void traverse() {
+    }
+}
