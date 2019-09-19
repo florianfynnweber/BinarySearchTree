5cc339ae4b02285b97728f16ba8ae7a04cf80e88
florianfynnweber <28307774+florianfynnweber@users.noreply.github.com>
2019-09-19 08:54:46 +0200

    Add graphviz (#4)
    * add new classes
    
    * fix code right, left with getter an setter
    
    * add graphiz implementation
    
    * add methods to bnode
    remove image
    
    * fix classes
    
    * add jw new classes
    add backup file
    
    * add method for graphiz

--- src/main/java/backend/BinarySearchTreeBCK.java
+++ src/main/java/backend/BinarySearchTreeBCK.java
@@ -1,25 +1,14 @@
 package backend;
 
-import guru.nidi.graphviz.attribute.*;
-import guru.nidi.graphviz.engine.Format;
-import guru.nidi.graphviz.engine.Graphviz;
-import guru.nidi.graphviz.model.Graph;
-
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static guru.nidi.graphviz.attribute.Label.Justification.LEFT;
-import static guru.nidi.graphviz.model.Factory.graph;
-import static guru.nidi.graphviz.model.Factory.node;
-import static guru.nidi.graphviz.model.Link.to;
-
-public class BinarySearchTree implements InterfaceBinarySearchTree {
-    BNode root;
+public class BinarySearchTreeBCK implements InterfaceBinarySearchTree {
+    Node<Node, Integer> root;
     ArrayList ordered;
 
-    public BinarySearchTree() {
+    public BinarySearchTreeBCK() {
         root = null;
     }
 
@@ -32,9 +21,9 @@ public class BinarySearchTree implements InterfaceBinarySearchTree {
         root = insertRec(root, value);
     }
 
-    public BNode insertRec(BNode tmp, Comparable value) {
+    public Node insertRec(Node tmp, Comparable value) {
         if (tmp == null) {
-            tmp = new BNode(value);
+            tmp = new Node(value);
             return tmp;
         } else {
             /* Otherwise, recur down the tree */
@@ -58,7 +47,7 @@ public class BinarySearchTree implements InterfaceBinarySearchTree {
 
     }
 
-    public BNode delete(BNode tmp, int key) {
+    public Node delete(Node tmp, int key) {
 
         if (tmp == null) return tmp;
 
@@ -88,7 +77,7 @@ public class BinarySearchTree implements InterfaceBinarySearchTree {
         return tmp;
     }
 
-    public int minValue(BNode tmp) {
+    public int minValue(Node tmp) {
         int minv = (int) tmp.getValue();
         while (tmp.getLeft() != null) {
             minv = (int) tmp.getLeft().getValue();
@@ -117,7 +106,7 @@ public class BinarySearchTree implements InterfaceBinarySearchTree {
      * @param key integer
      * @param tmp current root
      */
-    public BNode search(BNode tmp, int key) {
+    public Node search(Node tmp, int key) {
         // root is null or key is present at root
         if (tmp == null || (int) tmp.getValue() == key)
             return tmp;
@@ -164,7 +153,7 @@ public class BinarySearchTree implements InterfaceBinarySearchTree {
         return null;
     }
 
-    public void getInorder(BNode node) {
+    public void getInorder(Node node) {
         if (node == null)
             return;
         getInorder(node.getLeft());
@@ -173,7 +162,7 @@ public class BinarySearchTree implements InterfaceBinarySearchTree {
 
     }
 
-    public void getPostorder(BNode node) {
+    public void getPostorder(Node node) {
         if (node == null)
             return;
         getInorder(node.getLeft());
@@ -181,7 +170,7 @@ public class BinarySearchTree implements InterfaceBinarySearchTree {
         ordered.add(node.getValue());
     }
 
-    public void getPreorder(BNode node) {
+    public void getPreorder(Node node) {
         if (node == null)
             return;
         ordered.add(node.getValue());
@@ -189,29 +178,9 @@ public class BinarySearchTree implements InterfaceBinarySearchTree {
         getPreorder(node.getRight());
     }
 
-    public void toGraphiz() throws IOException {
-        guru.nidi.graphviz.model.Node main = node("main").with(Label.html("<b>main</b><br/>start"), Color.rgb("1020d0").font());
-        guru.nidi.graphviz.model.Node init = node(Label.markdown("**_init_**"));
-        guru.nidi.graphviz.model.Node execute = node("execute");
-        guru.nidi.graphviz.model.Node compare = node("compare").with(Shape.RECTANGLE, Style.FILLED, Color.hsv(.7, .3, 1.0));
-        guru.nidi.graphviz.model.Node mkString = node("mkString").with(Label.lines(LEFT, "make", "a", "multi-line"));
-        guru.nidi.graphviz.model.Node printf = node("printf");
-        Graph g = graph("example2").directed().with(
-                ((guru.nidi.graphviz.model.Node) main).link(
-                        to(node("parse").link(execute)).with(LinkAttr.weight(8)),
-                        to(init).with(Style.DOTTED),
-                        node("cleanup"),
-                        to(printf).with(Style.BOLD, Label.of("100 times"), Color.RED)),
-                execute.link(
-                        graph().with(mkString, printf),
-                        to(compare).with(Color.RED)),
-                init.link(mkString));
-        Graphviz.fromGraph(g).width(900).render(Format.PNG).toFile(new File("example/ex2.png"));
-    }
-
     public static void main(String[] args) throws BinarySearchTreeException, IOException {
-        BinarySearchTree tree = new BinarySearchTree();
-        tree.root = new BNode<Integer>(10);
+        BinarySearchTreeBCK tree = new BinarySearchTreeBCK();
+        tree.root = new Node<Node, Integer>(10);
         tree.addValue(5);
         tree.addValue(20);
         tree.addValue(25);
@@ -219,12 +188,10 @@ public class BinarySearchTree implements InterfaceBinarySearchTree {
         System.out.println(tree.hasValue(5));
         System.out.println(tree.hasValue(1000000));
         tree.addValue(3);
-        System.out.println(tree.traverse(Order.POSTORDER));
+        tree.traverse(Order.POSTORDER);
         tree.addValue(1);
         tree.delValue(23);
         System.out.println(tree.traverse(Order.INORDER));
-        System.out.println(tree.traverse(Order.POSTORDER));
-        System.out.println(tree.traverse(Order.PREORDER));
     }
 
 
