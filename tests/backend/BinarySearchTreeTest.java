d8a76fdeeb1583d54c6a73a3884b775aa95513af
Florian Weber <florian.fynn.weber@gmail.com>
2019-09-20 15:58:54 +0200

    add some test and travis

--- tests/backend/BinarySearchTreeTest.java
+++ tests/backend/BinarySearchTreeTest.java
@@ -1,22 +1,31 @@
 package backend;
 
+
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
-import org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.Matchers.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BinarySearchTreeTest{
+class BinarySearchTreeTest {
     private BinarySearchTree tree;
+
+    private void addNodesToTree(int i_) throws BinarySearchTreeException {
+        for (int i=0; i<i_;i++){
+            tree.addValue(new Random().nextInt(10));
+        }
+    }
     @BeforeEach
-    void setup(){
+    void setup() {
         this.tree = new BinarySearchTree();
     }
+
     @Test
     void addValue() throws BinarySearchTreeException {
         tree.addValue(5);
@@ -32,7 +41,11 @@ class BinarySearchTreeTest{
     }
 
     @Test
-    void hasValue() {
+    void hasValue() throws BinarySearchTreeException {
+        this.addNodesToTree(6);
+        tree.addValue(800);
+        assertTrue(tree.hasValue(800));
+        assertFalse(tree.hasValue(700));
     }
 
     @Test
@@ -45,16 +58,19 @@ class BinarySearchTreeTest{
 
     @Test
     void traverse() throws BinarySearchTreeException {
-        List<Integer> actual = Arrays.asList(1,2,3,4,5,5);
+        List<Integer> actual = Arrays.asList(1, 2, 3, 4, 5, 5);
         tree.addValue(5);
         tree.addValue(3);
         tree.addValue(2);
         tree.addValue(6);
         tree.addValue(7);
-        assertThat(tree.traverse(Order.PREORDER).toArray(), arrayContaining(5,3,2,6,7));
+        assertThat(tree.traverse(Order.PREORDER).toArray(), arrayContaining(5, 3, 2, 6, 7));
+        assertThat(tree.traverse(Order.POSTORDER).toArray(), arrayContaining(2, 3, 6, 7, 5));
+        assertThat(tree.traverse(Order.INORDER).toArray(), arrayContaining(2, 3, 5, 6, 7));
     }
+
     @AfterEach
-    void teardown(){
+    void teardown() {
         this.tree = null;
     }
 }
